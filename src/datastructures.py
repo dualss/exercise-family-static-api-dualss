5777288ae6a11f43cc176cd83fcec30caeee52f1
dualss <dariongduals@gmail.com>
2025-03-13 13:06:12 +0000

    Todos los endpoints funcionando y agregados dos fallos 400

--- src/datastructures.py
+++ src/datastructures.py
@@ -11,25 +11,33 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
         # example list of members
         self._members = []
+        
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
+        member["id"] = self._generateId()
+        self._members.append(member)
         pass
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        #hacer un for para recorrer self._members y encontrar el id que sea = luego cuando coincida hacer remove
+        for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)
+                break
+        return self._members
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        # igual que el delete pero en vez de hacer remove solo retornarlo
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
